db3888b8669cca465c31ee28f23972e8205771db
ibuergis <ian@buergis.com>
2023-12-22 15:31:17 +0100

    Added login and verify

--- handler/authentication.js
+++ handler/authentication.js
@@ -1,1 +1,81 @@
+const crypto = require('crypto');
+const fs = require("fs");
 const users = require('../data/users.json');
+
+function saveData(data) {
+    const json = JSON.stringify(data);
+    fs.writeFileSync('./data/users.json', json, 'utf-8');
+}
+
+function generateResponse(code, description, data) {
+    return { code, description, data };
+}
+
+function generateSalt() {
+    let randomString = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890^-?!$¨*"';
+    let salt = '';
+    for (let i = 0; i < 11; i += 1) {
+        salt += randomString.charAt(Math.floor(Math.random() * randomString.length-1));
+    }
+    return salt;
+}
+
+function authenticate(request, response) {
+    if (typeof request.body.username !== 'string') {
+        return generateResponse(400, 'username is not defined or not a string', null);
+    }
+    if (typeof request.body.password !== 'string') {
+        return generateResponse(400, 'password is not defined or not a string', null);
+    }
+    let correctUser = null;
+    for (let user of users.data) {
+        if (user.username === request.body.username) {
+            correctUser = user;
+        }
+    }
+    if (correctUser === null) {
+        return generateResponse(400, 'user ' + request.body.username + ' doesnt exist' , null);
+    }
+
+    const correctUserIndex = users.data.indexOf(correctUser)
+    // hashes the sent password and checks it
+    let hash = crypto.createHash('sha256').update(request.body.password).digest('hex');
+    if (hash === correctUser.password) {
+        // generates a salt to make a unique hash for the cookie, this is done to avoid a stolen cookie to be working forever.
+        // the hex will be regenerated everytime it checks for authentication. Reason is that on a password change all cookies
+        // get invalidated
+        const salt = generateSalt()
+        const cookieHash = crypto.createHash('sha256').update(hash + salt).digest('hex');
+        response.cookie('username', request.body.username);
+        response.cookie('authentication', cookieHash);
+        users.data[correctUserIndex].salt = salt
+        saveData(users)
+        return generateResponse(202, 'authenticated', null);
+    }
+
+    return generateResponse(401, 'password is wrong', null);
+}
+
+module.exports.authenticate = authenticate
+
+function cookieIsValid(cookies) {
+    if (cookies.authentication === undefined || cookies.username === undefined) {
+        return generateResponse(400, 'Cookies has not been found', null);
+    }
+
+    for (let user of users.data) {
+        if (user.username === cookies.username) {
+            const cookieHash = crypto.createHash('sha256').update(user.password + user.salt).digest('hex');
+
+            if (cookies.authentication === cookieHash) {
+                return generateResponse(202, 'Cookie is valid', null);
+            }
+            return generateResponse(401, 'Cookie is invalid', null);
+        }
+    }
+    return generateResponse(400, 'user saved in cookie doesnt exist', null);
+}
+
+module.exports.cookieIsValid = cookieIsValid
+
+
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require("cookie-parser");
 
 const port = 3000;
 const app = express();
+app.use(cookieParser());
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -40,6 +43,18 @@ app.delete('/tasks/:id', (request, response) => {
   response.send(responseJson);
 });
 
+app.post('/login', (request, response) => {
+  const loginData = request.body;
+  const responseJson = authenticationHandler.authenticate(request, response);
+  response.status(responseJson.code);
+  response.send(responseJson);
+});
+
+app.get('/verify', (request, response) => {
+  const responseJson = authenticationHandler.cookieIsValid(request.cookies);
+  response.status(responseJson.code);
+  response.send(responseJson);
+});
 app.listen(port, () => {
   console.log(`TaskAPI is running on port ${port}`); // eslint-disable-line no-console
 });
